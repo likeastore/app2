611a3d6821eaba277581f433ade21ce04512bdaa
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-10-25 09:31:04 +0200

    favorites routes

--- source/js/router.js
+++ source/js/router.js
@@ -4,6 +4,14 @@ function router(app) {
 			.state('feed', {
 				url: '/feed',
 				templateUrl: 'views/feed.html'
+			})
+			.state('topics', {
+				url: '/topics',
+				templateUrl: 'views/topics.html'
+			})
+			.state('favorites', {
+				url: '/favorites',
+				templateUrl: 'views/favorites.html'
 			});
 
 		$locationProvider.html5Mode(true);
